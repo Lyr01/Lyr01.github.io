67793352624922772df9fa830840dc22f47aa692
Lyr01 <79974123+Lyr01@users.noreply.github.com>
2021-12-28 12:50:31 +0100

    Added Video

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+
+
+const toggleVideo = (nomeVideo) => {
+    let myVideo = document.getElementById(nomeVideo);
+    myVideo.style.display = 'block';
+    myVideo.requestFullscreen();
+
+    document.getElementById(nomeVideo).addEventListener('fullscreenchange', (event) => {
+        // document.fullscreenElement will point to the element that
+        // is in fullscreen mode if there is one. If not, the value
+        // of the property is null.
+        if (document.fullscreenElement) {
+          console.log(`Element: ${document.fullscreenElement.id} entered fullscreen mode.`);
+        } else {
+          console.log('Leaving full-screen mode.');
+          myVideo.style.display = 'none';
+        }
+      });
+}   
+
+
